2895bb804eb10f8e32197a568fad1685cbba9dc3
Luan Ribeiro <luan.decastro@hoamtechnology.com>
2026-05-03 16:33:17 -0300

    feat(state-management): Adiciona crud padrão no base-service

--- src/shared/service/base.service.ts
+++ src/shared/service/base.service.ts
@@ -1,6 +1,7 @@
 import { inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { ENV_CONFIG } from '../../core/config/environment.config';
+import { Observable } from 'rxjs';
 
 export abstract class BaseService<T> {
   protected readonly http = inject(HttpClient);
@@ -12,4 +13,23 @@ export abstract class BaseService<T> {
     return `${this.config.baseUrl}/${this.path}`;
   }
 
+  get(path: string, params?: HttpParams): Observable<T | T[]> {
+    return this.http.get<T | T[]>(`${this.url}/${path}`, { params });
+  }
+
+  post<R>(body: R, path: string = ''): Observable<T> {
+    return this.http.post<T>(`${this.url}/${path}`, body);
+  }
+
+  put<R>(path: string, body: R): Observable<T> {
+    return this.http.put<T>(`${this.url}/${path}`, body);
+  }
+
+  patch<R>(path: string, body: Partial<R>): Observable<T> {
+    return this.http.patch<T>(`${this.url}/${path}`, body);
+  }
+
+  delete(path: string): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${path}`);
+  }
 }
