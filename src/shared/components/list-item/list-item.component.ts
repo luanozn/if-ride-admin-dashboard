33e3e8188929eb0513121b48469b733bcf7df64e
Luan Ribeiro <luan.decastro@hoamtechnology.com>
2026-08-16 17:29:31 -0300

    feat(state-management): faz o list-item genérico

--- src/shared/components/list-item/list-item.component.ts
+++ src/shared/components/list-item/list-item.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { Application } from '../../../features/applications/application-list/state/models/application.model';
+import { ShowableEntity } from '../../models/utils/showable-entity.model';
 
 @Component({
   selector: 'app-list-item',
@@ -13,8 +13,8 @@ import { Application } from '../../../features/applications/application-list/sta
   },
   templateUrl: './list-item.component.html'
 })
-export class ListItemComponent {
-  @Input({ required: true }) user!: Application;
+export class ListItemComponent<T extends ShowableEntity> {
+  @Input({ required: true }) user!: T;
 
   @Input() showDetails = false;
   @Input() showDeleteIcon = false;
